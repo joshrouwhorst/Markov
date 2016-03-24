610f78532ce4d6b756f5fecbd8eb2032b029bf88
Josh Rouwhorst <joshrouwhorst@Joshs-MacBook-Air.local>
2016-03-24 01:45:03 -0400

    Better prefix handling

--- Markov/index.js
+++ Markov/index.js
@@ -1,25 +1,36 @@
-exports.create = function(phraseList, prefixLength) {
+module.exports = function Markov(phraseList, options) {
     var fixes = {};
+    var defaultOptions = {
+        prefixLength: 1,
+        caseSensitive: false
+    };
+
+    function setup() {
+        if (!options) options = {};
+
+        for (var opt in defaultOptions) {
+            if (!options.hasOwnProperty(opt)) {
+                options[opt] = defaultOptions[opt];
+            }
+        }
+    }
 
     function getPrefix(phrase) {
-        var splitPhrase = phrase.split(' ');
+        var splitPhrase = phrase.split(/[\n ]/g);
         var prefix = '';
-        for (var i = 1; i <= prefixLength; i++) {
+        for (var i = 1; i <= options.prefixLength; i++) {
             var index = splitPhrase.length - i;
             if (index < 0) {
                 break;
             }
             prefix = splitPhrase[index] + ' ' + prefix;
         }
-        return prefix.trim();
+        return formatPrefix(prefix);
     }
 
     function generateSuffix(phrase, prefix) {
-        if (prefix === '') {
-            prefix = '_';
-        }
-
         var suffixes = fixes[prefix];
+
         if (!suffixes || suffixes.length === 0) {
             return phrase;
         }
@@ -45,33 +56,39 @@ exports.create = function(phraseList, prefixLength) {
         return phrase.trim();
     }
 
+    function formatPrefix(prefix) {
+        prefix = prefix.trim();
+        if (!options.caseSensitive) prefix = prefix.toLowerCase();
+        prefix = prefix.replace(/[^0-9a-zA-Z ]/g, '');
+        if (prefix === '') prefix = '_';
+        return prefix;
+    }
+
     function digest() {
         for (var i = 0; i < phraseList.length; i++) {
-            var splitPhrase = phraseList[i].split(' ');
+            var splitPhrase = phraseList[i].split(/[\n ]/g);
 
             for (var j = 0; j < splitPhrase.length; j++) {
+                if (splitPhrase[j] === '') {
+                    splitPhrase.splice(j--, 1);
+                    continue;
+                }
+                
                 var prefix = '';
-                for (var k = 1; k <= prefixLength; k++) {
+                for (var k = 1; k <= options.prefixLength; k++) {
                     if (j - k < 0) {
                         break;
                     }
                     prefix = splitPhrase[j - k] + ' ' + prefix;
                 }
-                prefix = prefix.trim();
-
-                if (prefix === '') {
-                    prefix = '_';
-                }
-
-                if (!fixes[prefix]) {
-                    fixes[prefix] = [];
-                }
-
+                prefix = formatPrefix(prefix);
+                if (!fixes[prefix]) fixes[prefix] = [];
                 fixes[prefix].push(splitPhrase[j]);
             }
         }
     }
 
+    setup();
     digest(phraseList);
     return generate();
 };
